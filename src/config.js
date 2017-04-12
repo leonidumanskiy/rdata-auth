fc4da4b30d125faf644271351d5ce8c3646b3c2b
Leonid Umanskiy <splatt@splatt.ru>
2017-04-12 14:22:33 -0500

    Changed default port to 8080

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const config = {
     all: {
         env: process.env.NODE_ENV || 'development',
         root: path.join(__dirname, '..'),
-        port: process.env.PORT || 9000,
+        port: process.env.PORT || 8080,
         ip: process.env.IP || '0.0.0.0',
         masterKey: requireProcessEnv('MASTER_KEY'),
         jwtSecret: requireProcessEnv('JWT_SECRET'),
@@ -42,8 +42,6 @@ const config = {
         }
     },
     production: {
-        ip: process.env.IP || undefined,
-        port: process.env.PORT || 8080,
         mongo: {
             uri: process.env.MONGODB_URI || 'mongodb://localhost/rdata-auth'
         }
