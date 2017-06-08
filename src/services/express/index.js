58aa2cbc86df9cf605988c2fc949e5d461c2b05c
Leonid Umanskiy <splatt@splatt.ru>
2017-06-08 15:02:08 -0500

    Version updated to 1.0.7: Included CORS into the application

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const compression = require('compression');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,6 +10,9 @@ const errorHandlers = require('../../middlewares/error-handlers');
 module.exports = function app(path, router){
 
     const app = express();
+
+    // Cross-origin resource sharing
+    app.use(cors());
 
     // body-parser
     app.use(bodyParser.json()); // support json encoded bodies
